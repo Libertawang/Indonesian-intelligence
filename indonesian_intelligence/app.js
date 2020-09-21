13db4b909be66d19fa2313a13275ac61c641ab6c
Libertawang <libertawang@.github.com>
2020-09-21 21:45:35 +1000

    remove express-sslify
    since it is not working

--- indonesian_intelligence/app.js
+++ indonesian_intelligence/app.js
@@ -3,10 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const enforce = require('express-sslify')
-
-app.use(enforce.express({trustProtoHeader: true}));
-app.use(express.static(path.join(__dirname, 'dist')));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
