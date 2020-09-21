c887b12745bc705ac5dc463fb12d7f51a87a2108
Libertawang <libertawang@.github.com>
2020-09-22 01:42:18 +1000

    Tri sslify again

--- indonesian_intelligence/app.js
+++ indonesian_intelligence/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var enforce = require('express-sslify');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +13,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
+app.use(express.static("public")) 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
